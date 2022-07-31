834bae164e02c6a59d37da11769a4c3130133e32
Serhiy Osinniy <Serhiy.Osinniy@genesys.com>
2022-07-31 10:45:26 +0300

    Add more statuses

--- report.py
+++ report.py
@@ -29,11 +29,13 @@ for l in lines:
   rsp = re.split('^[a-zA-Z]+-[0-9]+ - ', l)
   lsp = l.split(' - ')
 
-  rsp[1] = replaceStr(rsp[1], 'OPEN', '. (In progress)')
-  rsp[1] = replaceStr(rsp[1], 'IN PROGRESS', '. (In progress)')
-  rsp[1] = replaceStr(rsp[1], 'RESOLVED', '. (Done)')
-  rsp[1] = replaceStr(rsp[1], 'CLOSED', '. (Done)')
-  rsp[1] = replaceStr(rsp[1], 'READY FOR IMPLEMENTATION', '. (In progress)')
+  rsp[1] = replaceStr(rsp[1], ' OPEN', '. (In progress)')
+  rsp[1] = replaceStr(rsp[1], ' REOPENED', '. (In progress)')
+  rsp[1] = replaceStr(rsp[1], ' IN PROGRESS', '. (In progress)')
+  rsp[1] = replaceStr(rsp[1], ' CODE DONE', '. (In progress)')
+  rsp[1] = replaceStr(rsp[1], ' READY FOR IMPLEMENTATION', '. (In progress)')
+  rsp[1] = replaceStr(rsp[1], ' RESOLVED', '. (Done)')
+  rsp[1] = replaceStr(rsp[1], ' CLOSED', '. (Done)')
 
   d[lsp[0]] = ''.join(rsp)
 
